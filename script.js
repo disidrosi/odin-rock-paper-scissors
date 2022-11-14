efe1ad96e634b3ac83f1ba4424415c1f3341cc13
disidrosi <cavalli.tobia@gmail.com>
2022-11-14 23:32:54 +0100

    Add UI buttons and display results
    - Receive input from buttons instead of prompt.
    - Keep track of the current score and display on the page.
    - Display a message on page after each round showing the winner.

--- script.js
+++ script.js
@@ -1,106 +1,93 @@
 "use strict";
 
 // Declare constants and variables
-const gameKeywords = ['rock', 'paper', 'scissors'];
+const gameKeywords = ["rock", "paper", "scissors"];
+
+// Select buttons
+const playerRock = document.querySelector("#btn-rock");
+const playerPaper = document.querySelector("#btn-paper");
+const playerScissors = document.querySelector("#btn-scissors");
+
+// Add event listeners
+playerRock.addEventListener("click", (e) => playRound("rock"));
+playerPaper.addEventListener("click", (e) => playRound("paper"));
+playerScissors.addEventListener("click", (e) => playRound("scissors"));
+
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+const gameResult = document.getElementById("game-winner");
 
 // Return a random element from a given array
 function chooseArrayElement(keywords) {
-    return keywords[Math.floor(Math.random() * keywords.length)]
-};
+	return keywords[Math.floor(Math.random() * keywords.length)];
+}
 
 // Make a random choice between rock, paper, and scissors
 function getComputerChoice() {
-    return chooseArrayElement(gameKeywords)
-};
-
-// Check if user input matches one of the game keywords
-function checkPlayerChoice(userChoice) {
-    if (gameKeywords.includes(userChoice)) {
-        return true
-    } else {
-        return false
-    };
-};
-
-// Get the user choice from prompt and display a message if not valid
-function getPlayerChoice() {
-    let promptInput = prompt().toLowerCase();
-    if (checkPlayerChoice(promptInput)) {
-        return promptInput
-    } else {
-        console.log('You must choose between rock, paper, or scissors.');
-    };
-};
+	return chooseArrayElement(gameKeywords);
+}
 
 // Compare two choices and return 0 (lose), 1 (win), or 2 (tie)
 function compareChoices(computerSelection, playerSelection) {
-    switch (playerSelection) {
-        case 'rock':
-            if (computerSelection === 'rock') {
-                return 2
-            } else if (computerSelection === 'paper') {
-                return 0
-            } else if (computerSelection === 'scissors') {
-                return 1
-            };
-        case 'paper':
-            if (computerSelection === 'rock') {
-                return 1
-            } else if (computerSelection === 'paper') {
-                return 2
-            } else if (computerSelection === 'scissors') {
-                return 0
-            };
-        case 'scissors':
-            if (computerSelection === 'rock') {
-                return 0
-            } else if (computerSelection === 'paper') {
-                return 1
-            } else if (computerSelection === 'scissors') {
-                return 2
-            };
-    };
-};
+	switch (playerSelection) {
+		case "rock":
+			if (computerSelection === "rock") {
+				return 2;
+			} else if (computerSelection === "paper") {
+				return 0;
+			} else if (computerSelection === "scissors") {
+				return 1;
+			}
+		case "paper":
+			if (computerSelection === "rock") {
+				return 1;
+			} else if (computerSelection === "paper") {
+				return 2;
+			} else if (computerSelection === "scissors") {
+				return 0;
+			}
+		case "scissors":
+			if (computerSelection === "rock") {
+				return 0;
+			} else if (computerSelection === "paper") {
+				return 1;
+			} else if (computerSelection === "scissors") {
+				return 2;
+			}
+	}
+}
+
+let playerScore = 0;
+let computerScore = 0;
 
 // Compare the results of one round
 function checkRoundWinner(result) {
-    if (result === 0) {
-        console.log('Tie.');
-        return null
-    } else if (result === 1) {
-        console.log('Player wins.');
-        return 'player'
-    } else if (result === 2) {
-        console.log('Computer wins.');
-        return 'computer'
-    };
-};
+	if (result === 0) {
+		gameResult.textContent = "Tie.";
+		return null;
+	} else if (result === 1) {
+		playerScore += 1;
+		playerScoreDisplay.textContent = playerScore;
+		if (playerScore >= 5) {
+			gameResult.textContent = "Player wins the match.";
+		} else {
+			gameResult.textContent = "Player wins the round.";
+		}
+	} else if (result === 2) {
+		computerScore += 1;
+		computerScoreDisplay.textContent = computerScore;
+		gameResult.textContent = "Computer wins.";
+		if (computerScore >= 5) {
+			gameResult.textContent = "Computer wins the match.";
+		} else {
+			gameResult.textContent = "Computer wins the round.";
+		}
+	}
+}
 
 // Play one round between computer and player
-function playRound() {
-    const computerChoice = getComputerChoice();
-    const playerChoice = getPlayerChoice();
-    const result = compareChoices(computerChoice, playerChoice);
-    return checkRoundWinner(result)
-};
-
-// Play a game for a given number of rounds and keep score
-function game(rounds) {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 1; i <= rounds; i++) {
-        let roundResult = playRound();
-        if (roundResult === 'player') {
-            playerScore += 1;
-        } else if (roundResult === 'computer') {
-            computerScore += 1;
-        };
-    };
-    if (playerScore > computerScore) {
-        console.log('Player wins the game.');
-    } else if (playerScore < computerScore) {
-        console.log('Computer wins the game.');
-    } else if (playerScore === computerScore) {
-        console.log('Tied game.');
-    };
-};
+function playRound(playerChoice) {
+	const computerChoice = getComputerChoice();
+	const result = compareChoices(computerChoice, playerChoice);
+	return checkRoundWinner(result);
+}
